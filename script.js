3ee51f46873bd7007e44555930cb150af87b1428
Sloth I <github@slothwerks.com>
2021-06-19 19:54:01 -0400

    addVideo now is added to the form as a submission handler; updates based on testing

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const programs = [
 // Build a function that will show the current list of programs in the UI
 function updateProgramList() {
   const programList = document.getElementById("programList");
+  // Wipe out existing content
+  programList.innerHTML = "";
   // Go through programs; add programs to program list
   for (let i = 0; i < programs.length; i++) {
     const program = programs[i];
@@ -53,18 +55,25 @@ function loadProgram(id) {
 
 // Build a function that will add a new program to the programs list,
 // then update the program list in the UI
-function addVideo() {
+// This function will be a submission handler and will therefore
+// take an event object as an argument
+function addVideo(event) {
+  console.log("addVideo fired.");
+  // Prevent default refresh of app
+  event.preventDefault();
   // Grab user input from the form
-  const title = document.getElementById("programTitle").value;
-  const description = document.getElementById("programDescription").value;
-  const url = document.getElementById("programUrl").value.toLowerCase();
+  const title = document.getElementById("programTitle").value.trim();
+  const description = document.getElementById("programDescription").value.trim();
+  const link = document.getElementById("programUrl").value.trim();
+  // Clean url (remove additional data, leaving only the "root" link)
+  const url = cleanUrl(link);
   // Parse the url for the video ID
   let id, parts;
-  if (url.includes("youtube") && url.includes("watch")) {
+  if (url.includes("youtube") && url.includes("watch?v=")) {
     // This is a link pulled from the browser window
     parts = url.split("watch?v=");
     id = parts[1];
-  } else if (url.includes("youtu.be")) {
+  } else if (url.includes("youtu.be/")) {
     // This is a link pulled from the YouTube "share" feature
     parts = url.split("youtu.be/");
   } else {
@@ -79,12 +88,25 @@ function addVideo() {
       description,
       url
     }
+    console.log("We're saving this program data: ", program);
     // Add to the program list array
     programs.push(program);
     // Reload the list in the UI
     updateProgramList();
   }
 }
+
+// Build a helper function that removes additional data from the end of
+// a URL (the stuff starting with "&") and returns a url converted to lowercase
+// Takes a string website link as an argument
+function cleanUrl(url) {
+  const parts = url.split("&");
+  return parts[0].toLowerCase();
+}
+
+// Update form to include addVideo as submission handler
+const programForm = document.getElementById("programForm");
+programForm.onsubmit = addVideo;
 
 // Update program list upon load
 updateProgramList();
